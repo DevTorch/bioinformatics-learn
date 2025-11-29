88e4823662ff89b2b2623a94c6fe66b82d56496b
Vitaliy <torchez@yandex.ru>
2025-11-29 20:38:09 +0300

    First solutions

--- monthly_plan/week_one/fasta_functions.py
+++ monthly_plan/week_one/fasta_functions.py
@@ -1,6 +1,20 @@
 import os
+from typing import Any
+
 
 def read_fasta_file(file_path: str) -> dict:
+    """
+    Read a FASTA file and return a dictionary containing the sequences.
+
+    Parameters:
+    file_path (str): The path to the FASTA file to read.
+
+    Returns:
+    dict: A dictionary containing the sequences from the FASTA file.
+
+    Raises:
+    FileNotFoundError: If the file at the given path does not exist.
+    """
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"File {file_path} not found")
     with open(file_path, "r") as f:
@@ -18,8 +32,71 @@ def read_fasta_file(file_path: str) -> dict:
     return fasta_dict
 
 def gc_content(seq):
-    """Содержание GC в последовательности ДНК/РНК"""
-    return round((seq.count('G') + seq.count('C')) / len(seq) * 100)
+    """
+    Return the GC content of a given sequence.
+
+    The GC content is the percentage of G and C nucleotides in a given sequence.
+
+    Parameters:
+    seq (str): The sequence to calculate the GC content for.
+
+    Returns:
+    str: The GC content of the given sequence as a string with two decimal places.
+    """
+    return format((seq.count('G') + seq.count('C')) / len(seq) * 100, '.2f')
+
+def gc_content_dict(fasta_dict: dict) -> dict:
+    """
+    Return a dictionary with GC content for each sequence in a given FASTA file.
+    The returned dictionary is sorted by GC content in descending order.
+
+    Parameters:
+    fasta_dict (dict): A dictionary containing sequences from a FASTA file.
+
+    Returns:
+    dict: A dictionary with GC content for each sequence in the given FASTA file.
+
+    """
+    gc_content_dictionary = {label: gc_content(seq) for label, seq in fasta_dict.items()}
+    return dict(sorted(gc_content_dictionary.items(), key=lambda x: x[1], reverse=True))
 
 def get_fasta_len(fasta_dict: dict):
     return len(fasta_dict)
+
+def genomes_summary_len(fasta_dict: dict):
+    return sum(len(seq) for seq in fasta_dict.values())
+
+def genomes_len(fasta_dict: dict):
+    return {label: len(seq) for label, seq in fasta_dict.items()}
+
+def genome_len(seq: str):
+    return len(seq)
+
+def reverse_complement(seq: str):
+    # return seq[::-1].replace('A', 'T').replace('T', 'A').replace('C', 'G').replace('G', 'C')
+    mapping = str.maketrans("ATCG", "TAGC")
+    return seq.translate(mapping)[::-1]
+
+def get_subsequence(seq: str, start: int = 0, end: int = -1) -> str:
+    """
+    Return a subsequence of a given sequence.
+
+    Parameters:
+    seq (str): The sequence to get the subsequence from.
+    start (int): The start index of the subsequence. Defaults to 0.
+    end (int): The end index of the subsequence. Defaults to -1, which means the end of the sequence.
+
+    Returns:
+    str: The subsequence of the given sequence.
+    """
+    if end == -1:
+        end = len(seq)
+    return seq[start:end] if start < end < len(seq) else "Invalid indices"
+
+def get_info(seq: str):
+    return (
+        f'Length            : {len(seq)}\n'
+        f'GC content        : {gc_content(seq)}%\n'
+        f'Sequence          : {seq}\n'
+        f'Reverse complement: {reverse_complement(seq)}\n'
+    )
--- monthly_plan/week_one/main.py
+++ monthly_plan/week_one/main.py
@@ -1,17 +1,14 @@
-
-from monthly_plan.week_one.fasta_functions import read_fasta_file, gc_content, get_fasta_len
+import fasta_functions as ff
 
 base_path = '/mnt/c/Users/Torchez/PycharmProjects/bioinformatics-learn/'
 
 file_path = base_path + 'monthly_plan/week_one/data/ls_orchid.fasta'
-print(file_path)
-fasta_dict = read_fasta_file(file_path)
-for key, value in fasta_dict.items():
-    print(key, ':',  value)
 
-print('\nGC content:')
-for key, value in fasta_dict.items():
-    print(f'{key}: {gc_content(value)}%')
+fasta_dict = ff.read_fasta_file(file_path)
+# for key, value in fasta_dict.items():
+#     print(key, ':',  value)
 
-print('\nLength:')
-print(f'Length of FASTA: {get_fasta_len(fasta_dict)} chromosomes')
+fasta_get = fasta_dict.get('gi|2765603|emb|Z78478.1|PVZ78478 P.victoria 5.8S rRNA gene and ITS1 and ITS2 DNA')
+print(f'\nSubsequence: {ff.get_subsequence(fasta_get, 10, 50)}')
+print('\nGet info:')
+print(ff.get_info(fasta_dict.get('gi|2765603|emb|Z78478.1|PVZ78478 P.victoria 5.8S rRNA gene and ITS1 and ITS2 DNA')))
